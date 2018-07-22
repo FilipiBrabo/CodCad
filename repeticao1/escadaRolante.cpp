d1fc129cc06e2dc57d604e2e2047b4834de7bdde
Filipi <filipi.brabo@gmail.com>
2018-07-22 16:04:20 -0300

    escadaRolante.cpp adicionado

--- repeticao1/escadaRolante.cpp
+++ repeticao1/escadaRolante.cpp
@@ -3,26 +3,22 @@
 using namespace std;
 
 int main(){
-    int numPessoas, tempo, tempoAntes, tempoTotal = 0, tempoInicio;
+    int numPessoas, tempo, tempoAntes, tempoTotal = 10, tempoInicio;
     cin >> numPessoas;
     
     cin >> tempo;
     tempoAntes = tempo;
-    tempoInicio = tempo;
+    //tempoInicio = tempo;
 
-    for (int i = 0; i < numPessoas; i++){
+    for (int i = 1; i < numPessoas; i++){
         cin >> tempo;
-        if (tempo - tempoAntes <= 10){
-            tempoAntes = tempo;
-        }else{
-            tempoTotal += tempo + 10 - tempoAntes;
-            tempoInicio = tempo;
-        }
-        tempoAntes = tempo;
-    }
-    if (tempoTotal = 0)
-        tempoTotal = tempo+10;
-    
+		if (tempo - tempoAntes > 10)
+				tempoTotal += 10;
+		else
+			tempoTotal += tempo - tempoAntes;
+		tempoAntes = tempo;
+	}  
+
     cout << tempoTotal << endl;
     return 0;
 }
